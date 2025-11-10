747a9f168529cd79ed879bd63e6f94994847da23
Rezwanur Rahman <rezwanhritom1537@gmail.com>
2025-11-10 22:00:41 +0600

    Create helper.h

---
+++ helper.h
@@ -0,0 +1,17 @@
+// helper.h
+// --------------------------------------------------
+// A simple custom header file with function declarations.
+// --------------------------------------------------
+
+#ifndef HELPER_H
+#define HELPER_H
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
+#endif
